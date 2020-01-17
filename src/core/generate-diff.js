99e21c00a8dbd607eca9d9b2c2fa60b22ea57a67
Adil Asif <adil@adilasif.com>
2020-01-17 16:56:09 -0500

    Refactor generateDiff method to add JSON validation
    I didn't realize at first that the JSON.parse() method will fail handily and can break my comparison in a bunch of ways, so I added a validation method to ensure that it doesn't happen.
    
    If, for example, one of the shop listing files was left empty but still existed, JSON.parse() would fail, and the diff would fail, and we'd get a whole bunch of errors.

--- src/core/generate-diff.js
+++ src/core/generate-diff.js
@@ -5,6 +5,34 @@ import diff from 'deep-diff';
 const readFileAsync = util.promisify(fs.readFile);
 
 /**
+ * Validates a string as JSON.
+ *
+ * @param {string} jsonString - A string to validate as JSON.
+ *
+ * @returns {Object|false} A valid JSON object if validation was successful, or false if not.
+ */
+const validateJSON = (jsonString) => {
+  try {
+    const result = JSON.parse(jsonString);
+
+    /**
+     * This conditional is to handle non-exception-throwing cases.
+     *
+     * For example, neither JSON.parse(true) or JSON.parse(123) will throw an error, not they are
+     * not valid JSON for the purposes of our comparison. In addition, JSON.parse(null) returns
+     * null, and typeof null === 'object', so we need to check for truthiness as well.
+     */
+    if (result && typeof result === 'object') {
+      return result;
+    }
+
+    return false;
+  } catch (err) {
+    return false;
+  }
+};
+
+/**
  * Generates a diff object for a given shop's listings.
  *
  * @param {number} shopID - An Etsy shop ID.
@@ -15,7 +43,7 @@ const readFileAsync = util.promisify(fs.readFile);
 const generateDiff = async (shopID, newListings) => {
   const oldListings = await readFileAsync(`item-listings/${shopID}.json`, 'utf8');
 
-  const parsedOldListings = JSON.parse(oldListings);
+  const parsedOldListings = validateJSON(oldListings);
 
   if (!parsedOldListings) {
     return 'INVALID_JSON';
